e974df5052ec8db83b4441179e609093b04c0a59
Petrea Stefan <stefan.petrea20@gmail.com>
2023-08-30 16:50:19 +0300

    Lectia 1 done

---
+++ apps.js
@@ -0,0 +1,28 @@
+const para1 = document.getElementById("paragraph-1");
+const para2 = document.getElementById("paragraph-2");
+const para3 = document.getElementById("paragraph-3");
+const para4 = document.getElementById("paragraph-4");
+const para5 = document.getElementById("paragraph-5");
+
+const allParagraphs = document.getElementsByTagName('p');
+const titles = document.getElementsByTagName("h1");
+const sections = document.getElementsByTagName("section");
+const divs= document.getElementsByTagName("div");
+const aTags= document.getElementsByTagName("a");
+
+const div1=document.getElementsByClassName("div-1");
+const div2=document.getElementsByClassName("div-2");
+const para6=document.getElementsByClassName("paragraph-6");
+const title2=document.getElementsByClassName("title-2")
+const title3=document.getElementsByClassName("title-3")
+
+const firstId=document.querySelector("#first-id")
+const allIds=document.querySelectorAll("#first-id")
+const aside=document.querySelector(".my-aside")
+const tableHead=document.querySelector('[title="table-header"]')
+const allTableHead=document.querySelectorAll('[title="table-header"]')
+const allTableCells=document.querySelectorAll('td:nth-child(even)')
+
+
+
+
